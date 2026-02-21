13e0d09e16516cf5cd4e1f89aad2bd4db9c2c525
triptishakya-dev <triptishakya.2001@gmail.com>
2026-02-22 05:20:49 +0530

    feat: Initialize a basic Express application with a health check endpoint.

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+const app = express();
+
+
+const port = 3000;
+
+app.get("/api/test", (req, res) => {
+  res.send("Health is ok");
+});
+
+
+
+
+app.listen(port, () => {
+  console.log("my port is listening");
+});
